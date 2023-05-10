28f4c73dab9a343e581f6a6224fba60518b89462
AMAN GUPTA <91683228+AmanGupta6395@users.noreply.github.com>
2023-05-10 13:13:14 +0530

    Add files via upload

---
+++ patterns/PatternA.java
@@ -0,0 +1,14 @@
+package pattern;
+
+import java.sql.SQLOutput;
+
+public class PatternA {
+    public static void main(String[] args) {
+    for(int i=4;i>0;i--){
+        for(int j=0;j<i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
---
+++ patterns/invertedStar.java
@@ -0,0 +1,12 @@
+package pattern;
+
+public class invertedStar {
+    public static void main(String args[]){
+        for(int i =4;i>0;i--){
+            for(int j=i ; j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
